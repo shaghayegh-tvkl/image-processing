026b5ccb5011de5bfa2d9fe897970ac64b2eb7ad
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-05-17 14:32:36 +0430

    denoising added

--- ImageEnhancement/1.py
+++ ImageEnhancement/1.py
@@ -3,6 +3,9 @@ import os
 from skimage import exposure
 from skimage import io
 from skimage.transform import rescale
+from skimage.restoration import denoise_nl_means, estimate_sigma
+import numpy as np
+
 
 file_names = [];
 #load images
@@ -45,4 +48,22 @@ j = 0
 for image in resized_images:
     logarithmic_corrected = exposure.adjust_log(image, 1)
     io.imsave(f'./contrast/resized-{file_names[j]}',logarithmic_corrected)
-    j = j + 1    
+    j = j + 1   
+
+#Denoising     
+patch_kw = dict(patch_size=5,      
+                patch_distance=6,  
+                multichannel=True)
+j = 0
+for image in images:
+    sigma_est = np.mean(estimate_sigma(image, multichannel=True))
+    denoise = denoise_nl_means(image, h=0.8 * sigma_est, fast_mode=True,**patch_kw)
+    io.imsave(f'./denoising/{file_names[j]}',denoise)
+    j = j + 1
+
+j = 0
+for image in resized_images:
+    sigma_est = np.mean(estimate_sigma(image, multichannel=True))
+    denoise = denoise_nl_means(image, h=0.8 * sigma_est, fast_mode=True,**patch_kw)
+    io.imsave(f'./denoising/resized-{file_names[j]}',denoise)
+    j = j + 1
